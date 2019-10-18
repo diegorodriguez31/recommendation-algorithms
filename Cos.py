ea8c4d43274716f1625a3603a1da504c575ee5c9
Diego Rodriguez <diego.guilhem65@gmail.com>
2019-10-18 15:04:45 +0000

    Update Cos.py

--- Cos.py
+++ Cos.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 # Filtrage collabortatif
 import numpy as np
-import csv
 from math import sqrt
 from math import fabs
 from math import acos
-from Projet_Maths import lecture_csv
+from CentreUser import lecture_csv
 
 donnees = lecture_csv()
 
@@ -21,25 +20,56 @@ of item-based methods can be found in Sarwar et al.
 (2001)."""
 
 def cosinusSimilarite(user1, user2):
-    sommeNum = 0
-    sommeDenumUser1 = 0
-    sommeDenumUser2 = 0
-    for i in range(np.shape(donnees)[1]):
-        if donnees[user1][i] != -1 and donnees[user2][i] != -1:
-            sommeNum += donnees[user1][i] * donnees[user2][i]
-            sommeDenumUser1 += donnees[user1][i] ** 2
-            sommeDenumUser2 += donnees[user2][i] ** 2
-    return acos(sommeNum / sqrt(sommeDenumUser1 * sommeDenumUser2))
+        sommeNum = 0
+        sommeDenumUser1 = 0
+        sommeDenumUser2 = 0
+        for i in range(np.shape(donnees)[1]):
+            if donnees[user1][i] != -1 and donnees[user2][i] != -1:
+                sommeNum += donnees[user1][i] * donnees[user2][i]
+                sommeDenumUser1 += donnees[user1][i] ** 2
+                sommeDenumUser2 += donnees[user2][i] ** 2
+        resultat = acos(sommeNum / sqrt(sommeDenumUser1 * sommeDenumUser2))
+        return round(resultat,2)
 
+def moyenneItems(user):
+    somme = 0
+    nombre = 0
+    for i in range(np.shape(donnees)[1]):
+        if donnees[user][i] != -1:
+            somme += donnees[user][i]
+            nombre += 1
+    moyenne = somme/nombre
+    return moyenne
+
+def ponderationPremiere(user,item):
+    num = 0
+    den = 0
+    for i in range(np.shape(donnees)[0]):
+            if donnees[i][item] != -1:
+                sim = cosinusSimilarite(user,i)
+                num += sim * (donnees[i][item] - moyenneItems(i))
+                den += fabs(sim)
+    return round(moyenneItems(user) + num/den,2)
+    
+def ponderationSeconde(user, item):
+    num = 0
+    den = 0
+    for i in range(np.shape(donnees)[0]):
+        if donnees[i,item] != -1:       
+            num += donnees[user][i] * cosinusSimilarite(user,i)
+            den += fabs(cosinusSimilarite(user,i))
+    return num/den
+
+    
 def donneesCompletes():
     donneesCompletes = donnees
     for i in range(np.shape(donneesCompletes)[0]):
         for j in range(np.shape(donneesCompletes)[1]):
             if donneesCompletes[i][j] == -1:
-                donneesCompletes[i][j] = cosinusSimilarite(i,j)
+                donneesCompletes[i][j] = ponderationPremiere(i, j)     
                 print donneesCompletes[i][j]
     return donneesCompletes
 
-donneesCompletes = donneesCompletes()
-                        
-            
+#donneesCompletes = donneesCompletes()                        
+
+print donneesCompletes()
