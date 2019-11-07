72ae17a09afae40d13c92803d4ae52188941e11c
Clément Ginestet <clement.ginestet9@gmail.com>
2019-11-07 18:02:54 +0000

    Update tauKendall.py Le tau fonctionne, il manque les pondérations

--- tauKendall.py
+++ tauKendall.py
@@ -6,6 +6,7 @@ import csv
 from math import sqrt
 from math import fabs
 
+
 # Lire le fichier csv et le mettre dans un tableau
 def lecture_csv():
     n = 100
@@ -22,6 +23,7 @@ def lecture_csv():
             i += 1
     return donnees
 
+
 donnees = lecture_csv()
 
 """    Recuperer une selection d'informations sur laquelle va se baser le systeme de filtrage
@@ -34,38 +36,23 @@ facon d’agreger les notes attribuees par les utilisateurs similaires.
     Chercher des utilisateurs qui ont les memes comportements avec l'utilisateur a qui l'on souhaite faire des recommandations
     Utiliser les notes des utilisateurs similaires pour calculer une liste de recommandations pour cet utilisateur."""
 
-def tauKendall(user1,user2):
+def tauKendall(user1, user2):
+    lUser1 = []
+    lUser2 = []
+    for i in range(np.shape(donnees)[1]):
+        if donnees[user1][i] != -1 and donnees[user2][i] != -1:
+            lUser1.append(donnees[user1][i])
+            lUser2.append(donnees[user2][i])
+    n = len(lUser1)
     nbConcordant = 0
     nbDiscordant = 0
-    for i in range(np.shape(donnees)[1]):
-        for j in range(np.shape(donnees)[1]):
-            if donnees[user1][i] != -1 and donnees[user2][i] != -1 and donnees[user1][j] != -1 and donnees[user2][j] != -1:
-                if donnees[user1][i] > donnees[user2][i] and donnees[user1][j] > donnees[user2][j] or donnees[user1][i] < donnees[user2][i] and donnees[user1][j] < donnees[user2][j]:
-                    nbConcordant += 1
-                else:
-                    if donnees[user1][i] > donnees[user2][i] and donnees[user1][j] < donnees[user2][j] or donnees[user1][i] < donnees[user2][i] and donnees[user1][j] > donnees[user2][j]:
-                        nbDiscordant += 1
-    print nbConcordant
-    print nbDiscordant
-    return 2/(1000*(1000-1))*(nbConcordant - nbDiscordant)
+    for i in range(0,n):
+        for j in range(i,n):
+            if lUser1[i] > lUser2[i] and lUser1[j] > lUser2[j] or lUser1[i] < lUser2[i] and lUser1[j] < lUser2[j]:
+                nbConcordant += 1
+            if lUser1[i] > lUser2[i] and lUser1[j] < lUser2[j] or lUser1[i] < lUser2[i] and lUser1[j] > lUser2[j]:
+                nbDiscordant += 1
+    return (2.0 * (nbConcordant - nbDiscordant))/(n * (n - 1))
 
-"""def ponderationPremiere(user,item):
-    num = 0
-    den = 0
-    for i in range(np.shape(donnees)[0]):
-        if donnees[i][item] != -1:
-            num += tabSimilarite[user][i] * (donnees[i][item] - listeMoyennes[i])
-            den += fabs(tabSimilarite[user][i])
-    return round(listeMoyennes[user] + num/den,2)
-
-
-def donneesCompletes():
-    donneesCompletes = donnees
-    for i in range(np.shape(donneesCompletes)[0]):
-        for j in range(np.shape(donneesCompletes)[1]):
-            if donneesCompletes[i][j] == -1:
-                donneesCompletes[i][j] = tableauIntermediaire[i][j]
-                print donneesCompletes[i][j]
-    return donneesCompletes"""
-    
 print tauKendall(0,1)
+
